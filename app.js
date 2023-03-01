52fa74ec5542d53b707e45707aa4fa0ed06e7897
asgerbirk <asger.birk@gmail.com>
2023-03-01 11:56:30 +0100

    first

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const teams = [
 ]
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/time.html")
+    res.sendFile(__dirname + "/public/index.html")
 })
 
 app.get("/api/teams", (req, res) => {
