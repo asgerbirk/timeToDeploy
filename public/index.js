67f41a1415b8a232c7a2e28b1510611d2ea24ad4
asgerbirk <asger.birk@gmail.com>
2023-03-01 12:35:58 +0100

    first

--- public/index.js
+++ public/index.js
@@ -1,9 +1,27 @@
-fetch("/api/teams")
-    .then(response => response.json())
-    .then(result => {
+const teams = [
+    {id: 1, name: "Chelsea", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/52914.png"},
+    {id: 2, name: "Liverpool", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/7889.png"},
+    {id: 3, name: "Bayern Munchen", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50037.png"},
+    {id: 4, name: "Benfica", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50147.png"},
+    {id: 5, name: "Brugge", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50043.png"},
+    {id: 6, name: "Dortmund", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/52758.png"},
+    {id: 7, name: "Frankfurt", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50072.png"},
+    {id: 8, name: "Inter Milan", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50138.png"},
+    {id: 9, name: "Leipzig", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/2603790.png"},
+    {id: 10, name: "Manchester City", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/52919.png"},
+    {id: 11, name: "AC Milan", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50058.png"},
+    {id: 12, name: "Napoli", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50136.png"},
+    {id: 13, name: "Paris", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/52747.png"},
+    {id: 14, name: "Porto", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50064.png"},
+    {id: 15, name: "Real Madrid", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/50051.png"},
+    {id: 16, name: "Tottenham", imgURL: "https://img.uefa.com/imgml/TP/teams/logos/100x100/1652.png"}
+]
+
+
+
         const teamWrapperDiv = document.getElementById("teams")
 
-        result.data.forEach(team => {
+        teams.forEach(team => {
             const teamDiv = document.createElement("div");
 
             const teamName = document.createElement("name");
@@ -16,7 +34,7 @@ fetch("/api/teams")
 
             teamWrapperDiv.appendChild(teamDiv)
         })
-    })
+
 
 const countDownDiv = document.getElementById("countdown")
 
